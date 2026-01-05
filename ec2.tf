4749e3a76e56dff128445ff6770b510199479058
Sachinm1798 <sachinswatz1798@gmail.com>
2026-01-05 13:06:03 +0000

    launced ec2 instance with nginx hosted

--- ec2.tf
+++ ec2.tf
@@ -54,10 +54,12 @@ resource aws_security_group my_security_group {
 resource "aws_instance" "my_instance" {
           key_name = aws_key_pair.my_key.key_name
           security_groups = [aws_security_group.my_security_group.name]
-          instance_type = "t3.micro"
-          ami =  "ami-02b8269d5e85954ef"
+          instance_type = var.ec2_instance_type
+          ami =  var.ec2_ami_id
+          user_data = file("install-nginx.sh")
+#Run shell script at startup
           root_block_device {
-                    volume_size = 8
+                    volume_size = var.ec2_root_storage_size
                     volume_type = "gp3"
                              }
           tags = {
